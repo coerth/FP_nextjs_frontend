af800cb539f6b01bc6b62b26889837ff1d14b7e3
Morten Bendeke <mbendeke@hotmail.com>
2024-11-20 15:15:48 +0100

    navbar not static any longer

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import AuthButton from "./AuthButton";
@@ -11,19 +11,42 @@ const Navbar = () => {
   const { user } = useUser();
   const currentPath = usePathname();
   const [currentSymbolColor, setCurrentSymbolColor] = useState('');
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   const handleSymbolChange = (color: string) => {
     setCurrentSymbolColor(color);
   };
 
   const linkClasses = (path: string) => {
-    return currentPath?.startsWith(path)
+    return currentPath.startsWith(path)
       ? { borderBottom: `2px solid ${currentSymbolColor}` }
       : {};
   };
 
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+        setShowNavbar(false);
+      } else { // if scroll up show the navbar
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY); // remember current page location to use in the next move
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <div className="navbar bg-gray-800 text-white">
+    <div className={`navbar bg-gray-800 text-white fixed w-full transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'}`}>
       <div className="container mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
           <div className="flex items-center h-full">
