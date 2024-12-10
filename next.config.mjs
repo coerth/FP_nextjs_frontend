8a50d181f84526c90b941384ccbfd08fada70602
Morten Bendeke <mbendeke@hotmail.com>
2024-12-10 09:11:33 +0100

    ignore lint during build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['cards.scryfall.io'],
       },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
